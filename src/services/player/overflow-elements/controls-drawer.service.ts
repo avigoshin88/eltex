4da2886d2307e9e15865493289957397a2fdae75
artkryuchenkov <artem.kryuchenkov@swtecnn.com>
2024-09-10 21:39:24 +0300

    fix drawers

--- src/services/player/overflow-elements/controls-drawer.service.ts
+++ src/services/player/overflow-elements/controls-drawer.service.ts
@@ -1,11 +1,7 @@
 import { OverflowElementDrawer } from "../../../interfaces/overflow-element-builder";
 
 export class ControlsOverflowDrawerService implements OverflowElementDrawer {
-  draw(container: HTMLDivElement): void {
-    console.log('draw controls')
-  }
+  draw(container: HTMLDivElement): void {}
 
-  setOptions(options: unknown): void {
-    throw new Error("Method not implemented.");
-  }
+  setOptions(options: unknown): void {}
 }
--- src/services/player/overflow-elements/timeline-drawer.service.ts
+++ src/services/player/overflow-elements/timeline-drawer.service.ts
@@ -2,11 +2,7 @@ import { RangeDto } from "../../../dto/range";
 import { OverflowElementDrawer } from "../../../interfaces/overflow-element-builder";
 
 export class TimelineOverflowDrawer implements OverflowElementDrawer {
-  draw(container: HTMLDivElement): void {
-    throw new Error("Method not implemented.");
-  }
+  draw(container: HTMLDivElement): void {}
 
-  setOptions(ranges: RangeDto[]): void {
-    throw new Error("Method not implemented.");
-  }
+  setOptions(ranges: RangeDto[]): void {}
 }
