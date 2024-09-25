e3ca5ef9ebc69da0b06ab3732502e9bd43d98f56
artkryuchenkov <artem.kryuchenkov@swtecnn.com>
2024-09-25 21:31:14 +0300

    refactor timeline division steps

--- src/services/player/overflow-elements/timeline-drawer.service.ts
+++ src/services/player/overflow-elements/timeline-drawer.service.ts
@@ -2,33 +2,33 @@ import { Nullable } from "../../../types/global";
 import { RangeData } from "../../../types/range";
 import { TimelineClickCallback } from "../../../types/timeline";
 
-const divisionSteps = {
-  "5s": 5 * 1000,
-  "10s": 10 * 1000,
-  "20s": 20 * 1000,
-  "30s": 30 * 1000,
-  "1m": 1 * 60 * 1000,
-  "2m": 2 * 60 * 1000,
-  "5m": 5 * 60 * 1000,
-  "10m": 10 * 60 * 1000,
-  "15m": 15 * 60 * 1000,
-  "30m": 30 * 60 * 1000,
-  "1h": 1 * 60 * 60 * 1000,
-  "2h": 2 * 60 * 60 * 1000,
-  "6h": 6 * 60 * 60 * 1000,
-  "12h": 12 * 60 * 60 * 1000,
-  "1d": 1 * 24 * 60 * 60 * 1000,
-  "2d": 2 * 24 * 60 * 60 * 1000,
-  "1w": 7 * 24 * 60 * 60 * 1000,
-  "2w": 14 * 24 * 60 * 60 * 1000,
-  "1M": 30 * 24 * 60 * 60 * 1000,
-  "1Q": 3 * 30 * 24 * 60 * 60 * 1000,
-  "6M": 6 * 30 * 24 * 60 * 60 * 1000,
-  "1y": 365 * 24 * 60 * 60 * 1000,
-  "2y": 2 * 365 * 24 * 60 * 60 * 1000,
-  "5y": 5 * 365 * 24 * 60 * 60 * 1000,
-  "10y": 10 * 365 * 24 * 60 * 60 * 1000,
-};
+const divisionSteps = [
+  { scale: 0.004, step: 5 * 1000 }, // 5 секунд
+  { scale: 0.002, step: 10 * 1000 }, // 10 секунд
+  { scale: 0.001, step: 20 * 1000 }, // 20 секунд
+  { scale: 0.0005, step: 30 * 1000 }, // 30 секунд
+  { scale: 0.0002, step: 1 * 60 * 1000 }, // 1 минута
+  { scale: 0.0001, step: 2 * 60 * 1000 }, // 2 минуты
+  { scale: 0.00005, step: 5 * 60 * 1000 }, // 5 минут
+  { scale: 0.00002, step: 10 * 60 * 1000 }, // 10 минут
+  { scale: 0.00001, step: 15 * 60 * 1000 }, // 15 минут
+  { scale: 0.000005, step: 30 * 60 * 1000 }, // 30 минут
+  { scale: 0.000002, step: 1 * 60 * 60 * 1000 }, // 1 час
+  { scale: 0.000001, step: 2 * 60 * 60 * 1000 }, // 2 часа
+  { scale: 0.0000005, step: 6 * 60 * 60 * 1000 }, // 6 часов
+  { scale: 0.0000002, step: 12 * 60 * 60 * 1000 }, // 12 часов
+  { scale: 0.0000001, step: 1 * 24 * 60 * 60 * 1000 }, // 1 день
+  { scale: 0.00000005, step: 2 * 24 * 60 * 60 * 1000 }, // 2 дня
+  { scale: 0.00000002, step: 7 * 24 * 60 * 60 * 1000 }, // 1 неделя
+  { scale: 0.00000001, step: 14 * 24 * 60 * 60 * 1000 }, // 2 недели
+  { scale: 0.000000005, step: 1 * 30 * 24 * 60 * 60 * 1000 }, // 1 месяц
+  { scale: 0.000000002, step: 3 * 30 * 24 * 60 * 60 * 1000 }, // 1 квартал
+  { scale: 0.000000001, step: 6 * 30 * 24 * 60 * 60 * 1000 }, // полгода
+  { scale: 0.0000000005, step: 1 * 365 * 24 * 60 * 60 * 1000 }, // 1 год
+  { scale: 0.0000000002, step: 2 * 365 * 24 * 60 * 60 * 1000 }, // 2 года
+  { scale: 0.0000000001, step: 5 * 365 * 24 * 60 * 60 * 1000 }, // 5 лет
+  { scale: 0.00000000005, step: 10 * 365 * 24 * 60 * 60 * 1000 }, // 10 лет (добавлено соответствующее значение scale)
+];
 
 export class TimelineOverflowDrawer {
   private ranges: RangeData[] = [];
@@ -244,57 +244,9 @@ export class TimelineOverflowDrawer {
   private getDivisionStep(): number {
     const scaleFactor = this.scale;
 
-    if (scaleFactor > 0.004) {
-      return divisionSteps["5s"];
-    } else if (scaleFactor > 0.002) {
-      return divisionSteps["10s"];
-    } else if (scaleFactor > 0.001) {
-      return divisionSteps["20s"];
-    } else if (scaleFactor > 0.0005) {
-      return divisionSteps["30s"];
-    } else if (scaleFactor > 0.0002) {
-      return divisionSteps["1m"];
-    } else if (scaleFactor > 0.0001) {
-      return divisionSteps["2m"];
-    } else if (scaleFactor > 0.00005) {
-      return divisionSteps["5m"];
-    } else if (scaleFactor > 0.00002) {
-      return divisionSteps["10m"];
-    } else if (scaleFactor > 0.00001) {
-      return divisionSteps["15m"];
-    } else if (scaleFactor > 0.000005) {
-      return divisionSteps["30m"];
-    } else if (scaleFactor > 0.000002) {
-      return divisionSteps["1h"];
-    } else if (scaleFactor > 0.000001) {
-      return divisionSteps["2h"];
-    } else if (scaleFactor > 0.0000005) {
-      return divisionSteps["6h"];
-    } else if (scaleFactor > 0.0000002) {
-      return divisionSteps["12h"];
-    } else if (scaleFactor > 0.0000001) {
-      return divisionSteps["1d"];
-    } else if (scaleFactor > 0.00000005) {
-      return divisionSteps["2d"];
-    } else if (scaleFactor > 0.00000002) {
-      return divisionSteps["1w"];
-    } else if (scaleFactor > 0.00000001) {
-      return divisionSteps["2w"];
-    } else if (scaleFactor > 0.000000005) {
-      return divisionSteps["1M"];
-    } else if (scaleFactor > 0.000000002) {
-      return divisionSteps["1Q"];
-    } else if (scaleFactor > 0.000000001) {
-      return divisionSteps["6M"];
-    } else if (scaleFactor > 0.0000000005) {
-      return divisionSteps["1y"];
-    } else if (scaleFactor > 0.0000000002) {
-      return divisionSteps["2y"];
-    } else if (scaleFactor > 0.0000000001) {
-      return divisionSteps["5y"];
-    } else {
-      return divisionSteps["10y"];
-    }
+    const stepInfo = divisionSteps.find((step) => scaleFactor > step.scale);
+
+    return stepInfo?.step ?? divisionSteps[divisionSteps.length - 1].step;
   }
 
   private clickEventListener(event: MouseEvent): void {
